de20af2cf4fbdd76d52bf09e8b376c61c32c65ee
Max <m.karsten10@gmail.com>
2019-12-28 13:44:43 +0100

    commit 2

--- src/intex.jsx
+++ src/intex.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './reducer'
+import App from '.app'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer);
+
+render(
+    <Provider store="store">
+        
+    </Provider>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
